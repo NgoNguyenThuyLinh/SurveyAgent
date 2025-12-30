99b95d723ba5b8531ffd46df6eb4beae96643a87
ngonthuylinh <ngonthuylinh1@dtu.edu.vn>
2025-12-30 14:19:18 +0700

    Remove unused files

--- Backend/migrations/017_add_data_to_notifications.sql
+++
@@ -1,4 +0,0 @@
--- Migration 017: Add data column to notifications table for storing action metadata (tokens, etc)
--- Date: 2025-11-22
-
-ALTER TABLE notifications ADD COLUMN `data` JSON NULL DEFAULT NULL AFTER `related_id`;
